fabc4409d86c77a60f6992582a791dc7364cbfb2
Swarnjeet <swaranjeet.singh@meritnation.com>
2020-08-13 00:12:11 +0530

    infinite slider

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-let slideBox = document.querySelectorAll(".slideBox");
-let slide = document.querySelector(".slide");
-console.log(slideBox.length);
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+// Here we're going to move the active class between the slides. You can do this however you want, but for brevity I'm using JQuery.
 
-let indicator = document.querySelectorAll(".indicator div");
+// Get all the slides
+var slides = $('.slide');
 
-let size = slideBox[0].clientWidth;
-console.log(size);
+// Move the last slide before the first so the user is able to immediately go backwards
+slides.first().before(slides.last());
 
-currentSlide = 0;
-showSlides(currentSlide);
-indicator[currentSlide].classList.add("active");
-
-
-next.addEventListener("click", function() {
-	showSlides(currentSlide += 1);
+$('button').on('click', function() {
+  // Get all the slides again
+  slides = $('.slide');
+  // Register button
+  var button = $(this);
+  // Register active slide
+  var activeSlide = $('.active');
+  
+  // Next function
+  if (button.attr('id') == 'next') {
+    // Move first slide to the end so the user can keep going forward
+    slides.last().after(slides.first());
+    // Move active class to the right
+    activeSlide.removeClass('active').next('.slide').addClass('active');
+  }
+  
+  // Previous function
+  if (button.attr('id') == 'previous') {
+    // Move the last slide before the first so the user can keep going backwards
+    slides.first().before(slides.last());
+    // Move active class to the left
+    activeSlide.removeClass('active').prev('.slide').addClass('active');
+  }
 });
-
-prev.addEventListener("click", function() {
-	showSlides(currentSlide += -1);
-});
-
-
-function showSlides(n) {
-
-  slide.style.transform = 'translateX(' + (-size*currentSlide) + 'px)'; 	
-
-  	if(slideBox[currentSlide].classList.contains("last")) {
-  		
-  	}
-
-  	for(let i=0; i<slideBox.length; i++) {
-  		indicator[i].className = indicator[i].className.replace("active",  "");
-  	}
-
-  	indicator[currentSlide].classList.add("active");
-
-}
-
-for(let i=0; i<slideBox.length; i++) {
-	indicator[i].addEventListener("click", function() {
-		showSlides(currentSlide = i);
-		
-		
-	});
-}
